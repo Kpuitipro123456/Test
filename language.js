b41bd1ef2118f50f342c60ed71ba4d0b78235f42
Kpuitipro123456 <hoangquanvip10@gmail.com>
2024-05-09 21:14:50 +0700

    project-C

---
+++ language.js
@@ -0,0 +1,157 @@
+const translations ={
+    en: {
+        nav1 : "TOP",
+        nav2 : "PROBLEM",
+        nav3 : "SOLVING",
+        nav4 : "GALLERY",
+        nav5 : "PLANS",
+        nav6 : "REVIEW",
+        nav7 : "CONTACT",
+        
+        
+        about1 : "Customer Paint Point",
+        about2 : "Do you feel steady at the moment...?",
+        description1 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description2 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description3 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description4 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        
+        
+        solving1 :"Problem Solving",
+        description5 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        
+        
+        gallery1 : "Gallery",
+        
+        
+        pricing1 : "Pricing Plans",
+        pricingdescription : "Choose the perfect package for you",
+        pricingsummary1 : "For those getting started",
+        pricingfeature1 : "Feature #1",
+        pricingfeature2 : "Feature #2",
+        pricingfeature3 : "Feature #3",
+        pricingcost1 : "$68",
+        pricingmonth1 : "per month",
+        pricingbutton1 : "Subscribe",
+        pricingtext1 : "Minimum spen $816 over 12 months",
+        pricingsummary2 : "Upgrade with more features",
+        pricingfeature4 : "Feature #1",
+        pricingfeature5 : "Feature #2",
+        pricingfeature6 : "Feature #3",
+        pricingcost2 : "$100",
+        pricingmonth2 : "per month",
+        pricingbutton2 : "Subscribe",
+        pricingtext2 : "Minimum spen $1200 over 12 months",
+        pricingsummary3 : "Enjoy powerful performance",
+        pricingfeature7 : "Feature #1",
+        pricingfeature8 : "Feature #2",
+        pricingfeature9 : "Feature #3",
+        pricingcost3 : "$200",
+        pricingmonth3 : "per month",
+        pricingbutton3 : "Subscribe",
+        pricingtext3 : "Minimum spen $2400 over 12 months",
+        
+
+        review1 : "Customer's Review",
+        reviewdescription1 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        reviewdescription2 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        reviewdescription3 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+        contact1 : "Contact Us",
+
+    },
+
+    tw: {
+        nav1 : "顶部",
+        nav2 : "问题",
+        nav3 : "解决",
+        nav4 : "画廊",
+        nav5 : "计划",
+        nav6 : "审查",
+        nav7 : "接触",
+        
+        
+        about1 : "Customer Paint Point",
+        about2 : "Do you feel steady at the moment...?",
+        description1 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description2 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description3 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description4 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        
+        
+        solving1 :"Problem Solving",
+        description5 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        
+        
+        gallery1 : "Gallery",
+        
+        
+        pricing1 : "Pricing Plans",
+        pricingdescription : "Choose the perfect package for you",
+        pricingsummary1 : "For those getting started",
+        pricingfeature1 : "Feature #1",
+        pricingfeature2 : "Feature #2",
+        pricingfeature3 : "Feature #3",
+        pricingcost1 : "$68",
+        pricingmonth1 : "per month",
+        pricingbutton1 : "Subscribe",
+        pricingtext1 : "Minimum spen $816 over 12 months",
+        pricingsummary2 : "Upgrade with more features",
+        pricingfeature4 : "Feature #1",
+        pricingfeature5 : "Feature #2",
+        pricingfeature6 : "Feature #3",
+        pricingcost2 : "$100",
+        pricingmonth2 : "per month",
+        pricingbutton2 : "Subscribe",
+        pricingtext2 : "Minimum spen $1200 over 12 months",
+        pricingsummary3 : "Enjoy powerful performance",
+        pricingfeature7 : "Feature #1",
+        pricingfeature8 : "Feature #2",
+        pricingfeature9 : "Feature #3",
+        pricingcost3 : "$200",
+        pricingmonth3 : "per month",
+        pricingbutton3 : "Subscribe",
+        pricingtext3 : "Minimum spen $2400 over 12 months",
+        
+
+        review1 : "Customer's Review",
+        reviewdescription1 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        reviewdescription2 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        reviewdescription3 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+        contact1 : "Contact Us",
+    }
+}
+
+const languageSelectop = document.querySelector("select");
+let n1 = document.getElementById("n1");
+let n2 = document.getElementById("n2");
+let n3 = document.getElementById("n3");
+let n4 = document.getElementById("n4");
+let n5 = document.getElementById("n5");
+let n6 = document.getElementById("n6");
+let n7 = document.getElementById("n7");
+
+languageSelectop.addEventListener("change", (event) => {
+    setLanguage(event.target.value)
+})
+
+const setLanguage = (language) => {
+    if(language == "en"){
+        n1.innerText = translations.en.nav1;
+        n2.innerText = translations.en.nav2;
+        n3.innerText = translations.en.nav3;
+        n4.innerText = translations.en.nav4;
+        n5.innerText = translations.en.nav5;
+        n6.innerText = translations.en.nav6;
+        n7.innerText = translations.en.nav7;
+    }else if(language == "tw"){
+        n1.innerText = translations.tw.nav1;
+        n2.innerText = translations.tw.nav2;
+        n3.innerText = translations.tw.nav3;
+        n4.innerText = translations.tw.nav4;
+        n5.innerText = translations.tw.nav5;
+        n6.innerText = translations.tw.nav6;
+        n7.innerText = translations.tw.nav7;
+    }
+}
